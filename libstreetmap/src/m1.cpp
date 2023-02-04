56fe9d370bdd9ce1dfd557f352bd013f652bae50
hanzhe14 <hanzhe14@ug210.eecg.toronto.edu>
2023-02-04 18:05:10 -0500

    same thing except added the function names

--- libstreetmap/src/m1.cpp
+++ libstreetmap/src/m1.cpp
@@ -21,8 +21,8 @@
 #include <iostream>
 #include "m1.h"
 #include "StreetsDatabaseAPI.h"
-
-
+#include <vector>
+using namespace std;
 // loadMap will be called with the name of the file that stores the "layer-2"
 // map data accessed through StreetsDatabaseAPI: the street and intersection 
 // data that is higher-level than the raw OSM data). 
@@ -37,7 +37,9 @@
 // name.
 
 // Global variables
+
 std::vector<std::vector<StreetSegmentIdx>> intersection_street_segments;
+
 
 bool loadMap(std::string map_streets_database_filename) {
     bool load_successful = false; //Indicates whether the map has loaded 
@@ -51,11 +53,11 @@ bool loadMap(std::string map_streets_database_filename) {
     
     //for(auto i : getNumIntersections()){
         //for(auto j : getNumStreetSegments()){
-    for(int i; i < getNumIntersections(); i++){
-        for(int j; j < getNumStreetSegments(); j++){
-            intersection_street_segments[i].push_back(getIntersectionStreetSegment(i, j));
+    /*for(int i = 0; i < getNumIntersections(); i++){
+        for(int j = 0; j < getNumStreetSegments(); j++){
+            intersection_street_segments[i][j].push_back(getIntersectionStreetSegment(i, j));
         }
-    }
+    }*/
     
     //
 
@@ -65,15 +67,125 @@ bool loadMap(std::string map_streets_database_filename) {
     return load_successful;
 }
 
+// Returns the distance between two (lattitude,longitude) coordinates in meters
+// Speed Requirement --> moderate
+double findDistanceBetweenTwoPoints(LatLon point_1, LatLon point_2){
+
+}
+
+// Returns the length of the given street segment in meters
+// Speed Requirement --> moderate
+double findStreetSegmentLength(StreetSegmentIdx street_segment_id){
+
+}
+
+// Returns the travel time to drive from one end of a street segment 
+// to the other, in seconds, when driving at the speed limit
+// Note: (time = distance/speed_limit)
+// Speed Requirement --> high 
+double findStreetSegmentTravelTime(StreetSegmentIdx street_segment_id){
+    
+}
+
+// Returns all intersections reachable by traveling down one street segment 
+// from the given intersection (hint: you can't travel the wrong way on a 
+// 1-way street)
+// the returned vector should NOT contain duplicate intersections
+// Corner case: cul-de-sacs can connect an intersection to itself 
+// (from and to intersection on  street segment are the same). In that case
+// include the intersection in the returned vector (no special handling needed).
+// Speed Requirement --> high 
+std::vector<IntersectionIdx> findAdjacentIntersections(IntersectionIdx intersection_id){
+    
+}
+
+// Returns the geographically nearest intersection (i.e. as the crow flies) to 
+// the given position
+// Speed Requirement --> none
+IntersectionIdx findClosestIntersection(LatLon my_position){
+    
+}
+
+// Returns the street segments that connect to the given intersection 
+// Speed Requirement --> high
 std::vector<StreetSegmentIdx> findStreetSegmentsOfIntersection(IntersectionIdx intersection_id){
     std::vector<StreetSegmentIdx> ss_ids;
     //for(auto i : getNumIntersectionStreetSegment(intersection_id)){
-    for(int i = 0; i < getNumIntersectionStreetSegment(intersection_id); i++){
+    /*for(int i = 0; i < getNumIntersectionStreetSegment(intersection_id); i++){
         int ss_id = intersection_street_segments[intersection_id][i];
+        ss_ids.push_back(ss_id);
+    }*/
+    
+    for(int i = 0; i < getNumIntersectionStreetSegment(intersection_id); ++i) {
+        int ss_id = getIntersectionStreetSegment(intersection_id, i);
         ss_ids.push_back(ss_id);
     }
     
     return ss_ids;
+}
+
+// Returns all intersections along the a given street.
+// There should be no duplicate intersections in the returned vector.
+// Speed Requirement --> high
+std::vector<IntersectionIdx> findIntersectionsOfStreet(StreetIdx street_id){
+
+    
+}
+
+// Return all intersection ids at which the two given streets intersect
+// This function will typically return one intersection id for streets
+// that intersect and a length 0 vector for streets that do not. For unusual 
+// curved streets it is possible to have more than one intersection at which 
+// two streets cross.
+// There should be no duplicate intersections in the returned vector.
+// Speed Requirement --> high
+std::vector<IntersectionIdx> findIntersectionsOfTwoStreets(StreetIdx street_id1, StreetIdx street_id2){
+    
+}
+
+// Returns all street ids corresponding to street names that start with the 
+// given prefix 
+// The function should be case-insensitive to the street prefix. 
+// The function should ignore spaces.
+//  For example, both "bloor " and "BloOrst" are prefixes to 
+// "Bloor Street East".
+// If no street names match the given prefix, this routine returns an empty 
+// (length 0) vector.
+// You can choose what to return if the street prefix passed in is an empty 
+// (length 0) string, but your program must not crash if street_prefix is a 
+// length 0 string.
+// Speed Requirement --> high 
+std::vector<StreetIdx> findStreetIdsFromPartialStreetName(std::string street_prefix){
+    
+}
+
+// Returns the length of a given street in meters
+// Speed Requirement --> high 
+double findStreetLength(StreetIdx street_id){
+    
+}
+
+// Returns the nearest point of interest of the given type (e.g. "restaurant") 
+// to the given position
+// Speed Requirement --> none 
+POIIdx findClosestPOI(LatLon my_position, std::string POItype){
+    
+}
+
+// Returns the area of the given closed feature in square meters
+// Assume a non self-intersecting polygon (i.e. no holes)
+// Return 0 if this feature is not a closed polygon.
+// Speed Requirement --> moderate
+double findFeatureArea(FeatureIdx feature_id){
+    
+}
+
+// Return the value associated with this key on the specified OSMNode.
+// If this OSMNode does not exist in the current map, or the specified key is 
+// not set on the specified OSMNode, return an empty string.
+// Speed Requirement --> high
+std::string getOSMNodeTagValue (OSMID OSMid, std::string key){
+    
 }
 
 void closeMap() {
