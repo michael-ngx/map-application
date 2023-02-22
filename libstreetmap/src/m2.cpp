4b6ef5367ede5bbf6f9b8a8e4a27fbf823c2e0aa
Han  Zhengxuan <hanzhe14@ug157.eecg.toronto.edu>
2023-02-21 20:54:06 -0500

    Update for Milestone 2 by /cad2/ece297s/public/bin/ece297init for hanzhe14

---
+++ libstreetmap/src/m2.cpp
@@ -0,0 +1,31 @@
+/* 
+ * Copyright 2023 University of Toronto
+ *
+ * Permission is hereby granted, to use this software and associated 
+ * documentation files (the "Software") in course work at the University 
+ * of Toronto, or for personal use. Other uses are prohibited, in 
+ * particular the distribution of the Software either publicly or to third 
+ * parties.
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "m2.h"
+
+void drawMap() {
+   // Set up the ezgl graphics window and hand control to it, as shown in the 
+   // ezgl example program. 
+   // This function will be called by both the unit tests (ece297exercise) 
+   // and your main() function in main/src/main.cpp.
+   // The unit tests always call loadMap() before calling this function
+   // and call closeMap() after this function returns.
+}
